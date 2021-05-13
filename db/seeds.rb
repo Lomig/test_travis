e7ae170a997b6adb5f45a3a8d2bec4006945c331
Lomig Enfroy <lomig@hey.com>
2021-05-13 21:52:09 +0200

    Add Product Hunt list

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+puts 'Creating 5 products...'
+5.times do |i|
+  product = Product.create!(
+    name: Faker::Company.name,
+    tagline: Faker::Company.catch_phrase
+  )
+  puts "#{i + 1}. #{product.name}"
+end
+puts 'Finished!'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
